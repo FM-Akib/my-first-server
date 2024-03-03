22a809b5d603c72b70bf37b30983c3a0633ee94f
FM-Akib <muntasirakib08@gmail.com>
2024-03-03 20:01:30 +0600

    get from mongo

--- index.js
+++ index.js
@@ -31,11 +31,15 @@ async function run() {
 
     app.post('/users', async (req, res)=>{
         const user = req.body;
-        console.log("new user ", user);
-         const result = await userCollection.insertOne(user);
-         res.send(result);
+        const result = await userCollection.insertOne(user);
+        res.send(result);
     });
-
+     
+    app.get('/users', async (req, res)=>{
+      const cursor = userCollection.find()
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
 
 
